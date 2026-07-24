9d1a12e95eb9cf89f4d4a3f9c8f7f101944359bc
fatcat <dan.mcnulty@gmail.com>
2026-07-24 13:03:58 -0400

    ``` chore(dhcp): refresh NTP server defaults to pool.ntp.org current list ```

--- server/src/db/migrations/024_seed_dhcp_defaults.sql
+++ server/src/db/migrations/024_seed_dhcp_defaults.sql
@@ -2,4 +2,4 @@
 INSERT OR IGNORE INTO dhcp_option_defaults (option_code, value, updated_at)
 VALUES
   (51, '3600', datetime('now')),
-  (42, '155.248.196.28,23.186.168.129,23.186.168.128,216.229.4.66', datetime('now'));
+  (42, '157.245.125.229,45.79.214.107,137.190.2.4,23.157.160.168', datetime('now'));
